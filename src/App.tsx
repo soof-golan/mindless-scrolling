de8de365b262772539cfeb18a9d3ab44acd73ff9
soof-golan <83900570+soof-golan@users.noreply.github.com>
2022-01-28 01:34:55 +0200

    Update to image.png
    * remove header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './image.jpg';
+import logo from './image.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      {/*<header className="App-header">*/}
+      {/*  /!*<h1>ScrollTube</h1>*!/*/}
+      {/*</header>*/}
       <img src={logo} className='MindlessImage'/>
     </div>
   );
